8d75c44dfa662bbd06e18ced3c2c31f2efccdc12
Eliot Arntz <arntzel@gmail.com>
2013-10-29 22:21:27 +0100

    Initial Commit

---
+++ Overdue/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Overdue
+//
+//  Created by Eliot Arntz on 10/29/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
